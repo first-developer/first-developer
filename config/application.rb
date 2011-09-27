1653497282f90e4f7678557f42bd9418351dac8b
first-developer <lioninho11@hotmail.fr>
2011-09-27 03:59:37 +0200

    - fixed : restructured the layout of main template  - added : twitter bootstrap framework

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,10 @@
 require File.expand_path('../boot', __FILE__)
 
+
 require 'rails/all'
+
+
+
 
 # If you have a Gemfile, require the gems listed there, including any gems
 # you've limited to :test, :development, or :production.
@@ -14,6 +18,8 @@ module FirstDeveloper
 
     # Custom directories with classes and modules you want to be autoloadable.
     # config.autoload_paths += %W(#{config.root}/extras)
+    
+    
 
     # Only load the plugins named here, in the order given (default is alphabetical).
     # :all can be used as a placeholder for all plugins not explicitly named.
@@ -31,10 +37,15 @@ module FirstDeveloper
     # config.i18n.default_locale = :de
 
     # JavaScript files you want as :defaults (application.js is always included).
-    config.action_view.javascript_expansions[:defaults] = %w()
+    config.action_view.javascript_expansions[:defaults] = %w(jquery rails)
 
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
+    
+    #config generator 
+    config.generators do |g|
+      g.template_engine :erb
+    end
 
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
---
+++ public/javascripts/application.js
@@ -0,0 +1,6 @@
+$(function() {
+    
+    // Activate dropdown menu 
+    $('.topbar li.menu').dropdown();
+    
+});
