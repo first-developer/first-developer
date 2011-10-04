faf3ee8d2b5758c78fd474e4049c05c3fa36682c
first-developer <lioninho11@hotmail.fr>
2011-10-04 23:53:03 +0200

    - added : translation effect n fd_tut_go btn  - fixed : anmiations bug related to the previous addition

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -6,4 +6,13 @@ $(function() {
     //Activate tabs menu 
     $('.tabs').tabs();
     
+    //Animate .fd_tut_go btn 
+    $(".fd_tut_go").bind({
+      mouseover: function(){
+        $(this).animate({backgroundPosition: "-65px -158px"}, 100);
+      },
+      mouseout:function(){
+         $(this).animate({backgroundPosition: "-96px -158px"}, 100);
+      }
+    });
 });
