1f88762c8ae0fe6b83b0a5c9fef4c388d501a428
first-developer <lioninho11@hotmail.fr>
2011-10-06 01:44:11 +0200

    - added : datetime to each tut

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+  
+  
+  # time_tag(date_or_time, *args) :  Returns an html time tag for the given date or time.
+  # =============================================================================================
+  def time_tag(date_or_time, *args)
+    options  = args.extract_options!
+    format   = options.delete(:format) || :long
+    content  = args.first || I18n.l(date_or_time, :format => format)
+    datetime = date_or_time.acts_like?(:time) ? date_or_time.xmlschema : date_or_time.rfc3339
+
+    content_tag(:time, content, options.reverse_merge(:datetime => datetime))
+  end
 end
