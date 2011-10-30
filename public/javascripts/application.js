be390856962b0f360daeb5a9cf300b8e22c96a15
first-developer <lioninho11@hotmail.fr>
2011-10-30 02:40:56 +0100

    - added : checklist style list  - fixed : actions area style  - fixed : enhance the structure of the section layout

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,33 +1,34 @@
 $(function() {
     
-    // Activate dropdown menu 
-    // -----------------------------
-    $('.topbar li.menu').dropdown();
-    
-    // Activate tabs menu 
-    // ---------------------------
-    $('.tabs').pills();
-    
-    // Animate .fd_tut_go btn 
-    // ---------------------------
-    $(".fd_tut_go").slideBackground("-96px -158px", "-65px -158px" );
-    
-    
-    // Animate pagination btn
-    // ------------------------
-    $(".fd_pgb .fd_pgb_content .fd_pgb_left.enabled").slideBackground("-363px 0", "-439px 0", 200, 150 );
-    
-    $(".fd_pgb .fd_pgb_content .fd_pgb_right.enabled").slideBackground("-564px -5px", "-501px -5px", 200, 150 );
-    
-  
-    // Handle displaying of admin box
-	  // ---------------------------------
+  // Activate dropdown menu 
+  // -----------------------------
+  $('.topbar li.menu').dropdown();
+
+
+  // Activate tabs menu 
+  // ---------------------------------
+  $('.tabs').pills();
+
+
+  // Animate .fd_tut_go btn 
+  // ---------------------------------
+  $(".fd_tut_go").slideBackground("-96px -158px", "-65px -158px" );
+
+
+  // Animate pagination btn
+  // ---------------------------------
+  $(".fd_pgb .fd_pgb_content .fd_pgb_left.enabled").slideBackground("-363px 0", "-439px 0", 200, 150 );
+  $(".fd_pgb .fd_pgb_content .fd_pgb_right.enabled").slideBackground("-564px -5px", "-501px -5px", 200, 150 );
+      
+      
+  // Handle displaying of admin box
+	// ---------------------------------
 	  $(this).click(function() {	
 	    $(".fd_admin_action")
 	      .removeClass('fd_admin_action_hover') 
 	      .find(".fd_admin_action_content").hide();
 	  });	
-	
+
 	  //$(".fd_admin_action ul").hide();
 	
 	  $(".fd_admin_action").bind("click",function(e) {
@@ -46,7 +47,18 @@ $(function() {
 	    }  	  
 	  });
 
-
+  // Handle follow and unfollow acion
+  // ---------------------------------
+  /* $(".fd_follow_action.on").hover(
+    function() {
+      $(this).stop().animate({backgroundPosition : "-428px -32px"});
+    },
+    //callback
+    function() {
+      $(this).stop().animate({backgroundPosition : "-547px -32px"});
+    }
+  );*/
+    
 
     
     
